fa2e312ecb0320811d894eb7a5327dc1c7dd11c5
Rp70 <lequocdung99vn@yahoo.com>
2022-11-14 11:37:00 +0700

    feat: install-bproj.sh script

---
+++ install-bproj.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#set -ex
+
+
+read -r -p "Are you sure you want to install/upgrade .bproj in $PWD? [y/N]" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    git clone https://github.com/Rp70/dotbproj.git dotbproj # Clone this repo
+    rm -rdf ./.bproj # Remove the exising version
+    mv dotbproj/.bproj ./ # Move the .bproj to your project folder
+    rm -rdf dotbproj # Clean up the cloned repo
+    echo ".bproj installed. Thank you!"
+else
+    echo "Installation/Upgrade cancelled. Thank you!"
+fi
